4e5b15ac09c7c8ea24a24aa46c3e9cd25b85b9c1
Maciej Lewkowicz <lewkowicz@tuta.io>
2021-07-17 00:46:40 +0200

    User and folder structure creation. Mod download

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,11 +3,41 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
-include_recipe "ark"
 
 package %w(git inxi)
 
-ark "gitea" do
-  path "/var/lib/gitea"
-  url 'https://dl.gitea.io/gitea/1.8.1/gitea-1.8.1-linux-amd64'
+user 'git' do
+  home '/home/git'
+  shell '/bin/bash'
+end
+
+%w( /var /var/lib /var/lib/gitea /var/lib/gitea ).each do |path|
+  directory path do
+  owner 'git'
+  group 'root'
+  mode  '0750'
+  end
+end
+
+%w{custom data log}.each do |dir|
+  directory "/var/lib/gitea/#{dir}" do
+    mode '750'
+    owner 'git'
+    group 'root'
+  end
+end
+
+directory '/etc/gitea' do
+  owner 'git'
+  group 'root'
+  mode  '770'
+end
+
+execute 'gitea export' do
+  command "/bin/bash -c 'export GITEA_WORK_DIR=/var/lib/gitea/'"
+end
+
+remote_file '/usr/local/bin/gitea' do
+  source 'https://dl.gitea.io/gitea/1.8.1/gitea-1.8.1-linux-amd64'
+  mode '0755'
 end
